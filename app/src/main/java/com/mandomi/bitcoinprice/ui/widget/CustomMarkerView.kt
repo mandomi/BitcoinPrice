0588c281658b5abc695c5e3c092a25c892faf45b
Arash Mandomi <arash.mandomy@gmail.com>
2018-11-11 01:19:00 +0330

    Refactor MarkerView and add date to the merker

--- app/src/main/java/com/mandomi/bitcoinprice/ui/widget/CustomMarkerView.kt
+++ app/src/main/java/com/mandomi/bitcoinprice/ui/widget/CustomMarkerView.kt
@@ -8,19 +8,21 @@ import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.highlight.Highlight
 import com.github.mikephil.charting.utils.MPPointF
 import com.github.mikephil.charting.utils.Utils
+import com.mandomi.bitcoinprice.extension.toMinuteDayMonthYear
 import kotlinx.android.synthetic.main.custom_marker_view.view.*
 
 @SuppressLint("ViewConstructor")
 class CustomMarkerView(context: Context, layoutResource: Int) : MarkerView(context, layoutResource) {
 
-    override fun refreshContent(e: Entry?, highlight: Highlight?) {
-        if (e is CandleEntry) {
-            val ce = e as CandleEntry?
-            value.text = Utils.formatNumber(ce!!.high, 0, true)
+    override fun refreshContent(entry: Entry, highlight: Highlight?) {
+        if (entry is CandleEntry) {
+            value.text = Utils.formatNumber(entry.high, 0, true)
+            date.text = entry.low.toLong().toMinuteDayMonthYear()
         } else {
-            value.text = Utils.formatNumber(e!!.y, 0, true)
+            value.text = Utils.formatNumber(entry.y, 0, true)
+            date.text = entry.x.toLong().toMinuteDayMonthYear()
         }
-        super.refreshContent(e, highlight)
+        super.refreshContent(entry, highlight)
     }
 
     override fun getOffset(): MPPointF {
