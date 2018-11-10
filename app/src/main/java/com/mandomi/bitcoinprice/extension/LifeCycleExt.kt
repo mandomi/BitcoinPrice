4cff9726fa8b9cf5b7e88b841d83d3a60d39cd73
Arash Mandomi <arash.mandomy@gmail.com>
2018-11-10 16:38:23 +0330

    Create LifeCycleExt for life cycle's extention methods

---
+++ app/src/main/java/com/mandomi/bitcoinprice/extension/LifeCycleExt.kt
@@ -0,0 +1,9 @@
+package com.mandomi.bitcoinprice.extension
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import com.mandomi.bitcoinprice.ui.model.Resource
+
+fun <T : Any, L : LiveData<Resource<T>>> LifecycleOwner.observe(liveData: L, body: (Resource<T>?) -> Unit) =
+    liveData.observe(this, Observer(body))
