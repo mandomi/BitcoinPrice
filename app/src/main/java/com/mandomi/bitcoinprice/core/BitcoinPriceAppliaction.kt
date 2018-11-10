9b1589155dbe3139c16e5e7eaa66731ba541d642
Arash Mandomi <arash.mandomy@gmail.com>
2018-11-10 13:21:25 +0330

    Create BitcoinPrice application

---
+++ app/src/main/java/com/mandomi/bitcoinprice/core/BitcoinPriceAppliaction.kt
@@ -0,0 +1,10 @@
+package com.mandomi.bitcoinprice.core
+
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
+
+class BitcoinPriceAppliaction : DaggerApplication() {
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
