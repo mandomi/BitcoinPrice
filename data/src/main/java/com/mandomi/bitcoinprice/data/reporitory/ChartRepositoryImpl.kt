29fc468517556c35427f83db4759d2a4a4230852
Arash Mandomi <arash.mandomy@gmail.com>
2018-11-10 12:13:06 +0330

    Implement Chart Repository

---
+++ data/src/main/java/com/mandomi/bitcoinprice/data/reporitory/ChartRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package com.mandomi.bitcoinprice.data.reporitory
+
+import com.mandomi.bitcoinprice.data.datasource.ChartRemoteDataSource
+import com.mandomi.bitcoinprice.data.toChart
+import com.mandomi.bitcoinprice.domain.entity.Chart
+import com.mandomi.bitcoinprice.domain.repository.ChartRepository
+import io.reactivex.Single
+import javax.inject.Inject
+
+class ChartRepositoryImpl @Inject constructor(private val chartRemoteDataSource: ChartRemoteDataSource) :
+    ChartRepository {
+
+    override fun getChart(chartName: String, timeSpan: String, rollingAverage: String, start: String): Single<Chart> {
+        return chartRemoteDataSource.getChart(chartName, timeSpan, rollingAverage, start).map { it.toChart() }
+    }
+
+}
