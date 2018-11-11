c15c1850916043bb720b841dfcdf07e43e0d14d6
Arash Mandomi <arash.mandomy@gmail.com>
2018-11-11 20:38:48 +0330

    Make ChartViewModel test scenarios

--- app/src/test/java/com/mandomi/bitcoinprice/viewmodel/ChartViewModelUnitTest.kt
+++ app/src/test/java/com/mandomi/bitcoinprice/viewmodel/ChartViewModelUnitTest.kt
@@ -1,15 +1,14 @@
 package com.mandomi.bitcoinprice.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.mandomi.bitcoinprice.boundary.toChartItem
 import com.mandomi.bitcoinprice.domain.entity.Chart
+import com.mandomi.bitcoinprice.domain.faliure.Failure
 import com.mandomi.bitcoinprice.domain.interactor.GetMarketPriceChartUseCase
 import com.mandomi.bitcoinprice.factory.ChartFactory
 import com.mandomi.bitcoinprice.ui.chart.ChartViewModel
 import com.mandomi.bitcoinprice.ui.model.ResourceState
-import io.mockk.clearAllMocks
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
+import io.mockk.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -32,20 +31,120 @@ class ChartViewModelUnitTest {
     @Test
     fun `getMarketPriceChartUseCase executes`() {
         viewModel.load("7days")
-
         verify(exactly = 1) { getMarketPriceChart.execute(any(), any(), any()) }
     }
 
     @Test
-    fun `getMarketPriceChartUseCase returns success`() {
-        viewModel.load("")
+    fun `getMarketPriceChartUseCase success`() {
+        val success = slot<(chart: Chart) -> Unit>()
+
         every {
-            val success = captureLambda<(result: Chart) -> Unit>()
-            val error = captureLambda<(e: Throwable) -> Unit>()
-            getMarketPriceChart.execute(GetMarketPriceChartUseCase.Params(), success, error)
+            getMarketPriceChart.execute(any(), capture(success), any())
+        } answers {
             success.invoke(ChartFactory.makeChart(10))
         }
+
+        viewModel.load("")
         assert(viewModel.getData().value?.resourceState == ResourceState.SUCCESS)
+    }
+
+    @Test
+    fun `getMarketPriceChartUseCase success and contains data`() {
+        val chart: Chart = ChartFactory.makeChart(10)
+        val success = slot<(chart: Chart) -> Unit>()
+
+        every {
+            getMarketPriceChart.execute(any(), capture(success), any())
+        } answers {
+            success.invoke(chart)
+        }
+
+        viewModel.load("")
+        assert(viewModel.getData().value?.resourceState == ResourceState.SUCCESS)
+        assert(viewModel.getData().value?.data == chart.toChartItem())
+    }
+
+    @Test
+    fun `getMarketPriceChartUseCase success and contains no error`() {
+        val chart: Chart = ChartFactory.makeChart(10)
+        val success = slot<(chart: Chart) -> Unit>()
+
+        every {
+            getMarketPriceChart.execute(any(), capture(success), any())
+        } answers {
+            success.invoke(chart)
+        }
+
+        viewModel.load("")
+        assert(viewModel.getData().value?.resourceState == ResourceState.SUCCESS)
+        assert(viewModel.getData().value?.failure == null)
+    }
+
+    @Test
+    fun `getMarketPriceChartUseCase fails`() {
+        val error = slot<(throwable: Throwable) -> Unit>()
+
+        every {
+            getMarketPriceChart.execute(any(), any(), capture(error))
+        } answers {
+            error.invoke(Failure.ServerError("Server Error"))
+        }
+
+        viewModel.load("")
+        assert(viewModel.getData().value?.resourceState == ResourceState.ERROR)
+    }
+
+    @Test
+    fun `getMarketPriceChartUseCase fails and contains error data`() {
+        val error = slot<(throwable: Throwable) -> Unit>()
+
+        every {
+            getMarketPriceChart.execute(any(), any(), capture(error))
+        } answers {
+            error.invoke(Failure.ServerError("Server Error"))
+        }
+
+        viewModel.load("")
+        assert(viewModel.getData().value?.resourceState == ResourceState.ERROR)
+        assert(viewModel.getData().value?.failure is Failure.ServerError)
+    }
+
+    @Test
+    fun `getMarketPriceChartUseCase fails and contain no data`() {
+        val error = slot<(throwable: Throwable) -> Unit>()
+
+        every {
+            getMarketPriceChart.execute(any(), any(), capture(error))
+        } answers {
+            error.invoke(Failure.ServerError("Server Error"))
+        }
+
+        viewModel.load("")
+
+        assert(viewModel.getData().value?.resourceState == ResourceState.ERROR)
+        assert(viewModel.getData().value?.data == null)
+    }
+
+    @Test
+    fun `getMarketPriceChartUseCase returns loading`() {
+        viewModel.load("")
+        assert(viewModel.getData().value?.resourceState == ResourceState.LOADING)
+    }
+
+    @Test
+    fun `getMarketPriceChartUseCase contains no data when loading`() {
+        viewModel.load("")
+
+        assert(viewModel.getData().value?.resourceState == ResourceState.LOADING)
+        assert(viewModel.getData().value?.data == null)
+    }
+
+    @Test
+    fun `getMarketPriceChartUseCase contains no error when loading`() {
+        viewModel.load("")
+
+        assert(viewModel.getData().value?.resourceState == ResourceState.LOADING)
+        assert(viewModel.getData().value?.failure == null)
     }
 
 }
