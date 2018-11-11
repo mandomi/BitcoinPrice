532b29992dcb32b3c6bf8b2e36ab6f238d2b67bf
Arash Mandomi <arash.mandomy@gmail.com>
2018-11-11 18:07:47 +0330

    Refactor ChartRepositoryTest class

--- data/src/test/java/com/mandomi/bitcoinprice/data/repository/ChartRepositoryImplTest.kt
+++ data/src/test/java/com/mandomi/bitcoinprice/data/repository/ChartRepositoryImplTest.kt
@@ -1,10 +1,10 @@
 package com.mandomi.bitcoinprice.data.repository
 
+import com.mandomi.bitcoinprice.data.datasource.ChartRemoteDataSource
 import com.mandomi.bitcoinprice.data.factory.ChartEntityFactory
 import com.mandomi.bitcoinprice.data.factory.ChartEntityFactory.CHART_NAME
 import com.mandomi.bitcoinprice.data.factory.ChartEntityFactory.TIME_SPAN
 import com.mandomi.bitcoinprice.data.factory.ChartEntityFactory.WRONG_CHART_NAME
-import com.mandomi.bitcoinprice.data.datasource.ChartRemoteDataSource
 import com.mandomi.bitcoinprice.data.reporitory.ChartRepositoryImpl
 import com.mandomi.bitcoinprice.data.toChart
 import com.mandomi.bitcoinprice.domain.faliure.Failure
@@ -26,18 +26,20 @@ class ChartRepositoryImplTest {
     }
 
     @Test
-    fun `getChart works`() {
-
+    fun `getChart returns data`() {
         every { dataSource.getChart(CHART_NAME, any()) } returns Single.just(ChartEntityFactory.chart)
-        every { dataSource.getChart(WRONG_CHART_NAME, any()) } returns Single.error(Failure.NotFoundError("Not Found"))
-
         val successObserver = repository.getChart(CHART_NAME, TIME_SPAN).test()
         with(successObserver) {
             assertComplete()
             assertNoErrors()
             assertValue(ChartEntityFactory.chart.toChart())
         }
+    }
 
+    @Test
+    fun `getChart returns error`() {
+        every { dataSource.getChart(WRONG_CHART_NAME, any()) } returns Single.error(Failure.NotFoundError("Not Found"))
         repository.getChart(WRONG_CHART_NAME, TIME_SPAN).test().assertError(Failure.NotFoundError::class.java)
     }
+
 }
